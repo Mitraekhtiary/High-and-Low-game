ef356a3dcae4f46bc05708d26e84e22298ff3783
Mitraekhtiary <140202013+Mitraekhtiary@users.noreply.github.com>
2023-07-23 18:39:18 +0430

    HighAndLow.py

--- .github/workflows/HighAndLow.py
+++ .github/workflows/HighAndLow.py
@@ -1,39 +1,35 @@
 import random
+
 def High_Low(level, low, high):
-    number = random.randint(low, high)
-    print(f"Level {level}: Guess a number between {low} and {high}")
-    guesses = 1
-    #number_loop
-    while True:
-        guess = low + (high-low)//2
-        answer = int(input("Enter your guess: "))
-
-        if answer < guess:
-            print("Please enter higher number.")
-            low = guess + 1
-        elif answer > guess:
-            print("Please enter lower number.")
-            high = guess - 1
-        elif answee == guess: 
-            print("Good job! you've done it right^^")
-            break
-        else:
-            print("(Invalid option!)")
-            break
-
-# levels
-levels = {
-    1: (1, 10),
-    2: (1, 100),
-    3: (1, 500),
-    4: (1, 1000)
-}
-
-# choose_level
-Level = int(input("Choose a level (1-4): "))
+ number = random.randint(low, high)
+ print(f"level {level}: Guess a number between {low} and {high}" )
+ guesses=1
+ while True:
+    guess = low + (high - low)//2
+    answer = int(input("enter your number:"))
+    if answer < guess:
+        print ("please enter higher number!")
+        low = guess + 1
+    elif answer > guess:
+        print ("please enter lower number!")
+        high = guess - 1
+    elif answer == guess:
+        print ("good job! you entered true answer!")
+        break
+    else:
+        print ("you entered invalid number!")
+        break
+#levels
+levels = {1: (1, 10),
+          2: (1, 100),
+          3: (1, 500),
+          4: (1, 1000),
+          }
+Level = int(input("Choose you level (1-4):"))
 level_range = levels.get(Level)
-if isinstance(level_range, tuple):
-    High_Low(Level, level_range[0], level_range[1])
+
+if isinstance (level_range, tuple):
+    High_Low (Level, level_range [0], level_range [1])
 else:
-    print("Invalid level!")
+    print ("Invalid level")
 
